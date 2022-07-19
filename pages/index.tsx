fe5c99e04882df8f0ebcfc46b9733721808b34e7
Nikita Tikhonov <stillonov@gmail.com>
2022-07-19 22:55:24 +0300

    Project creation

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+import styles from '../styles/Home.module.css';
+
+const Home: NextPage = () => {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>rakimov.ru</title>
+                <meta name="description" content="" />
+            </Head>
+            Hello, World!
+        </div>
+    );
+};
+
+export default Home;
