986c5ea11b993d5c49a9e8b5073aa6e7873352ea
Nikita Tikhonov <stillonov@gmail.com>
2022-08-11 23:17:57 +0300

    Add footer content

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -11,7 +11,8 @@ export const Footer: React.FC<Props> = () => (
     <footer className={styles.root}>
         <Container>
             <Divider />
-            Page Footer
+            &copy;&nbsp;2022 Айдар Ракимов
+            <a href='mailto:aydar@rakimov.ru'>aydar@rakimov.ru</a>
         </Container>
     </footer>
 );
