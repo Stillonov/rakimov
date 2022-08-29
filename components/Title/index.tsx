3c1b23852884ced4c8b078a7b45d221f7e235d4a
Nikita Tikhonov <stillonov@gmail.com>
2022-08-29 19:08:03 +0300

    Add page header

--- components/Title/index.tsx
+++ components/Title/index.tsx
@@ -1,13 +1,13 @@
 import cn from 'classNames';
 import styles from './styles.module.css';
 
-interface ButtonProps {
+interface TitleProps {
     children: React.ReactNode;
     className?: string;
     level?: string;
 }
 
-export const Title: React.FC<ButtonProps> = ({ children, className = '', level = '1' }) => {
+export const Title: React.FC<TitleProps> = ({ children, className = '', level = '1' }) => {
     const CustomElement = `h${level}` as React.ComponentType | keyof JSX.IntrinsicElements;
 
     return <CustomElement className={cn(styles.root, [styles[`level${level}`]], className)}>{children}</CustomElement>
