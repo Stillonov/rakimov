be229a5af3a064549deb5a352fc854433f247811
Nikita Tikhonov <stillonov@gmail.com>
2022-08-29 19:06:51 +0300

    Add page header

---
+++ components/PageHeader/index.tsx
@@ -0,0 +1,21 @@
+import cn from 'classNames';
+import styles from './styles.module.css';
+
+interface PageHeaderProps {
+    compact?: boolean;
+    leftSlot?: React.ReactNode;
+    rightSlot?: React.ReactNode;
+    className?: string;
+}
+
+export const PageHeader: React.FC<PageHeaderProps> = ({ leftSlot, rightSlot, compact = false, className = '' }) => {
+    return (
+        <div className={cn(styles.root, {[styles.compact]: compact}, className)}>
+            <div className={styles.gridContainer}>
+                {leftSlot && <div className={styles.gridColumn}>{leftSlot}</div>}
+
+                {rightSlot && <div className={styles.gridColumn}>{rightSlot}</div>}
+            </div>
+        </div>
+    );
+};
--- pages/design/index.tsx
+++ pages/design/index.tsx
@@ -5,23 +5,33 @@ import { Layout } from 'components/Layout';
 import { Container } from 'components/Container';
 import { Breadcrumb } from 'components/Breadcrumb';
 import { BreadcrumbItem } from 'components/BreadcrumbItem';
+import { Title } from 'components/Title';
+import { PageHeader } from 'components/PageHeader';
 
 const Design: NextPageWithLayout = () => {
     return (
-        <div>
+        <>
             <Head>
                 <title>rakimov.ru</title>
                 <meta name="description" content="Design" />
             </Head>
 
             <Container>
-                <Breadcrumb>
-                    <BreadcrumbItem link="/">Главная</BreadcrumbItem>
-                    <BreadcrumbItem link="/design">Дизайню</BreadcrumbItem>
-                    <BreadcrumbItem link="/design/chudo">Чудо</BreadcrumbItem>
-                </Breadcrumb>
+                <PageHeader
+                    leftSlot={
+                        <>
+                            <Breadcrumb>
+                                <BreadcrumbItem link="/">Главная</BreadcrumbItem>
+                                <BreadcrumbItem link="/design">Дизайню</BreadcrumbItem>
+                                <BreadcrumbItem link="/design/chudo">Чудо</BreadcrumbItem>
+                            </Breadcrumb>
+
+                            <Title level='2'>Дизайню</Title>
+                        </>
+                    }
+                />
             </Container>
-        </div>
+        </>
     );
 };
 
