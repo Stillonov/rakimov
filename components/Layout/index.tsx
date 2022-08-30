8a411a24143bd69a0ce0e00582bed93fef3a6f37
Nikita Tikhonov <stillonov@gmail.com>
2022-08-30 17:29:36 +0300

    Add layout background image

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -2,6 +2,7 @@ import { Header } from 'components/Header';
 import { Footer } from 'components/Footer';
 
 import styles from './styles.module.css';
+import noiseImage from 'public/images/noise.png';
 
 interface LayoutProps {
     children: React.ReactNode;
@@ -9,7 +10,8 @@ interface LayoutProps {
 }
 
 export const Layout: React.FC<LayoutProps> = ({ children, backgroundImageUrl }) => {
-    const backgroundImage = `linear-gradient(180deg, rgba(19, 19, 21, 0.87) 0%, #131315 100%), url(${backgroundImageUrl})`;
+    // const backgroundImage = `linear-gradient(180deg, rgba(19, 19, 21, 0.1) 50%, #131315 100%), url(${backgroundImageUrl})`;
+    const backgroundImage = `url(${noiseImage.src}), url(${backgroundImageUrl})`;
 
     return (
         <div className={styles.root}>
